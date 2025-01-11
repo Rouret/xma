e26aa09e0ce9e714f26bb28f996ae19f0ee0d6db
Lucas Rouret <lucas.rouretperso@gmail.com>
2025-01-11 18:35:00 +0100

    update conf

--- conf.lua
+++ conf.lua
@@ -1,4 +1,8 @@
 function love.conf(t)
-    t.window.title = "Xma" 
+    t.title = "Xma"
+    t.author = "Rouret Lucas" 
+
+    t.window.title = "Xma"
+    t.window.icon = "sprites/icon.png"
     t.window.fullscreen = true
 end
