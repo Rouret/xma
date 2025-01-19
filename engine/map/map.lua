2c40ff7e8566ea490818ed7f980f734055ab84a3
Lucas Rouret <lucas.rouretperso@gmail.com>
2025-01-19 09:52:27 +0100

    refacto map

--- engine/map/map.lua
+++ engine/map/map.lua
@@ -14,76 +14,72 @@ end
 local Map = {}
 Map.__index = Map
 
-local TILE_SIZE = 32
-local MAP_WIDTH = 1000
-local MAP_HEIGHT = 1000
-local NOISE_SCALE = 2
-
 function Map.new(world)
     print("Creating map...")
     local self = setmetatable({}, Map)
 
     self.world = world
-    self.layers = {}
-    self.MAP_WIDTH = MAP_WIDTH
-    self.MAP_HEIGHT = MAP_HEIGHT
+    self.MAP_WIDTH = 1000
+    self.MAP_HEIGHT = 1000
+    self.TILE_SIZE = 32
+    self.NOISE_SCALE = 2
 
     self:generate()
-    self:generateElements()
 
     return self
 end
 
 function Map:convertPxToTile(px)
-    return math.floor(px / TILE_SIZE)
+    return math.floor(px / self.TILE_SIZE)
 end
+
 -- Génère la carte
 function Map:generate()
     print("Generating map...")
     -- Générer les cartes de bruit
-    local altitudeMap = NoiseUtils.generateNoiseMap(MAP_WIDTH, MAP_HEIGHT, NOISE_SCALE)
-    local humidityMap = NoiseUtils.generateNoiseMap(MAP_WIDTH, MAP_HEIGHT, NOISE_SCALE)
+    local altitudeMap = NoiseUtils.generateNoiseMap(self.MAP_WIDTH, self.MAP_HEIGHT, self.NOISE_SCALE)
+    local humidityMap = NoiseUtils.generateNoiseMap(self.MAP_WIDTH, self.MAP_HEIGHT, self.NOISE_SCALE)
 
     -- Assigner les biomes
-    self.biomes = BiomeGenerator.assignBiomes(MAP_WIDTH, MAP_HEIGHT, altitudeMap, humidityMap) or {}
+    self.biomes = BiomeGenerator.assignBiomes(self.MAP_WIDTH, self.MAP_HEIGHT, altitudeMap, humidityMap) or {}
+    self.tiles = {}
+    self.elements = {}
 
     -- Générer le terrain
-    self.layers[0] = self:generateTerrain()
+    self:generateTerrain()
+    -- Générer les éléments
+    self:generateElements()
 end
 
 function Map:generateTerrain()
     print("Generating terrain...")
-    local tiles = {}
 
-    for y = 1, MAP_HEIGHT do
-        tiles[y] = {}
-        for x = 1, MAP_WIDTH do
+    for y = 1, self.MAP_HEIGHT do
+        self.tiles[y] = {}
+        for x = 1, self.MAP_WIDTH do
             local biome = self.biomes[y][x]
             if biome then
                 local biomeModule = BiomeRegistry.getBiome(biome.name)
                 if biomeModule then
-                    tiles[y][x] = biomeModule.generateTerrain(x, y)
+                    self.tiles[y][x] = biomeModule.generateTerrain(x, y)
                 end
             end
         end
     end
-
-    return tiles
 end
 
 function Map:generateElements()
     print("Generating elements...")
-    self.elements = {}
 
-    for y = 1, MAP_HEIGHT do
-        for x = 1, MAP_WIDTH do
+    for y = 1, self.MAP_HEIGHT do
+        for x = 1, self.MAP_WIDTH do
             local biome = self.biomes[y][x]
             if biome then
                 local biomeModule = BiomeRegistry.getBiome(biome.name)
                 if biomeModule and love.math.random() < biomeModule.spawnProbability then
                     local element = biomeModule.generateElement(x, y)
                     if element then
-                        element.biomeName = biome.name -- Associer l'élément à son biome
+                        element.biomeName = biome.name
                         table.insert(self.elements, element)
                     end
                 end
@@ -95,39 +91,40 @@ end
 function Map:draw()
     local camX, camY, camWidth, camHeight = Camera.i:getVisibleArea()
 
-    local startX = math.max(1, math.floor(camX / TILE_SIZE))
-    local endX = math.min(MAP_WIDTH, math.ceil((camX + camWidth) / TILE_SIZE))
-    local startY = math.max(1, math.floor(camY / TILE_SIZE))
-    local endY = math.min(MAP_HEIGHT, math.ceil((camY + camHeight) / TILE_SIZE))
+    local startX = math.max(1, math.floor(camX / self.TILE_SIZE))
+    local endX = math.min(self.MAP_WIDTH, math.ceil((camX + camWidth) / self.TILE_SIZE))
+    local startY = math.max(1, math.floor(camY / self.TILE_SIZE))
+    local endY = math.min(self.MAP_HEIGHT, math.ceil((camY + camHeight) / self.TILE_SIZE))
 
-    -- Dessiner les terrains
     for y = startY, endY do
         for x = startX, endX do
             local biome = self.biomes[y][x]
             if biome then
                 local biomeModule = BiomeRegistry.getBiome(biome.name)
                 if biomeModule then
-                    local tile = self.layers[0][y][x]
-                    love.graphics.draw(biomeModule.terrain, tile.quad, (x - 1) * TILE_SIZE, (y - 1) * TILE_SIZE)
+                    local tile = self.tiles[y][x]
+                    love.graphics.draw(
+                        biomeModule.terrain,
+                        tile.quad,
+                        (x - 1) * self.TILE_SIZE,
+                        (y - 1) * self.TILE_SIZE
+                    )
                 end
             end
         end
     end
 
-    -- Dessiner les éléments
     self:drawElements(startX, endX, startY, endY)
 end
 
 function Map:drawElements(startX, endX, startY, endY)
-    local total = 0
     for _, element in ipairs(self.elements) do
         -- Vérifiez si l'élément est visible dans la zone de la caméra
         if
-            element.x >= startX * TILE_SIZE and element.x <= endX * TILE_SIZE and element.y >= startY * TILE_SIZE and
-                element.y <= endY * TILE_SIZE
+            element.x >= startX * self.TILE_SIZE and element.x <= endX * self.TILE_SIZE and
+                element.y >= startY * self.TILE_SIZE and
+                element.y <= endY * self.TILE_SIZE
          then
-            total = total + 1
-
             -- Récupérer le module de biome pour dessiner l'élément
             local biomeModule = BiomeRegistry.getBiome(element.biomeName)
             if biomeModule and biomeModule.drawElement then
